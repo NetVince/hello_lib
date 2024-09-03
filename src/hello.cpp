bc81b00eee4760ac7c5eb309c2558157d82a2c34
NetVince <vincent.auroyer@laposte.net>
2024-09-03 04:22:33 -0400

    Premier commit de la lib Hello

---
+++ src/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "hello.h"
+
+std::string current_version="1.0";
+
+void hello(){
+    std::cout << "C'est la lib HELLO/"<< current_version<< "\n" << std::endl;
+}
+
+void hello_print_vector(const std::vector<std::string> &strings) {
+    for(std::vector<std::string>::const_iterator it = strings.begin(); it != strings.end(); ++it) {
+        std::cout << "hello/1.0 " << *it << std::endl;
+    }
+}
